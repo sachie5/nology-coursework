93e92951b69199775d621dad11e0b47628ff663f
sachie5 <sachamccormack@gmail.com>
2023-10-27 16:51:15 +0100

    Completed function 3

--- 01-Tooling/02-VersionControl/02-ModuleProject/dist/javaproject.dev.js
+++ 01-Tooling/02-VersionControl/02-ModuleProject/dist/javaproject.dev.js
@@ -2,7 +2,7 @@
 
 /**Write a function that takes a number and returns true 
 if it is a positive number and false if it is a negative number.*/
-var positiveOrNegative = function positiveOrNegative(num) {
+var isNumberPositive = function isNumberPositive(num) {
   if (num < 0) {
     console.log(false);
   } else {
@@ -10,9 +10,9 @@ var positiveOrNegative = function positiveOrNegative(num) {
   }
 };
 
-positiveOrNegative(-1);
-positiveOrNegative(3);
-positiveOrNegative(-20);
+isNumberPositive(-1);
+isNumberPositive(3);
+isNumberPositive(-20);
 /**Write a function that takes a number of days 
  * and converts it into an age. */
 
@@ -24,3 +24,14 @@ var convertDaysToAge = function convertDaysToAge(days) {
 convertDaysToAge(9739);
 convertDaysToAge(52782);
 convertDaysToAge(7482);
+/**Write a function that takes three numbers 
+ and returns the largest of the three numbers. */
+
+var getLargestNumber = function getLargestNumber(num1, num2, num3) {
+  var largest = Math.max(num1, num2, num3);
+  console.log("The largest number is ".concat(largest, "."));
+};
+
+getLargestNumber(2, 9, 10);
+getLargestNumber(434, 942, 882);
+getLargestNumber(2973, 9812, 9182);
--- 01-Tooling/02-VersionControl/02-ModuleProject/javaproject.js
+++ 01-Tooling/02-VersionControl/02-ModuleProject/javaproject.js
@@ -1,6 +1,6 @@
 /**Write a function that takes a number and returns true 
 if it is a positive number and false if it is a negative number.*/
-const positiveOrNegative = (num) => {
+const isNumberPositive = (num) => {
         if (num < 0){
         console.log (false);
     } else {
@@ -8,9 +8,9 @@ const positiveOrNegative = (num) => {
     }
 }
 
-positiveOrNegative(-1);
-positiveOrNegative(3);
-positiveOrNegative(-20)
+isNumberPositive(-1);
+isNumberPositive(3);
+isNumberPositive(-20)
 
 /**Write a function that takes a number of days 
  * and converts it into an age. */
@@ -23,3 +23,15 @@ const convertDaysToAge = (days) => {
 convertDaysToAge (9739);
 convertDaysToAge (52782);
 convertDaysToAge (7482);
+
+/**Write a function that takes three numbers 
+ and returns the largest of the three numbers. */
+
+ const getLargestNumber = (num1, num2, num3) => {
+    const largest = Math.max (num1, num2, num3);
+    console.log(`The largest number is ${largest}.`)
+ }
+
+ getLargestNumber (2, 9, 10);
+ getLargestNumber (434, 942, 882);
+ getLargestNumber (2973, 9812, 9182);
